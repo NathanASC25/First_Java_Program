84bef606af7b311cf1ac662ce11dfe574f7651ca
NathanASC25 <javanathanjchin@gmail.com>
2025-08-29 20:47:15 -0400

    Latest Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld{
     public static void main(String[] args){
         String message = "Hello World!";
+        double num = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+        System.out.println("\n" + num);
+        
         String firstWord = giveFirstWord(message);
         System.out.println(firstWord);
     }
