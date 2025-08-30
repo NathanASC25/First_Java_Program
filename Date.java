a00929e9610b5b4f7240c04e28eb074f95ba80de
NathanASC25 <javanathanjchin@gmail.com>
2025-08-30 00:29:16 -0400

    Another commit

---
+++ Date.java
@@ -0,0 +1,10 @@
+public class Date{
+    public static void main(String[] args){
+        int day = 30;
+        int month = 8;
+        int year = 2025;
+        // Date format: MM/DD/YYYY
+        String date = month + "/" + day + "/" + year;
+        System.out.println("\n" + date);
+    }
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,13 @@
-public class HelloWorld{
-    public static void main(String[] args){
-        String message = "Hello World!";
-        double num = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
-        System.out.println("\n" + num);
+// public class HelloWorld{
+//     public static void main(String[] args){
+//         String message = "Hello World!";
+//         double num = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+//         System.out.println("\n" + num);
         
-        String firstWord = giveFirstWord(message);
-        System.out.println(firstWord);
-    }
-    public static String giveFirstWord(String msg){
-        return msg.substring(0, msg.indexOf(" "));
-    }
-}
+//         String firstWord = giveFirstWord(message);
+//         System.out.println(firstWord);
+//     }
+//     public static String giveFirstWord(String msg){
+//         return msg.substring(0, msg.indexOf(" "));
+//     }
+// }
